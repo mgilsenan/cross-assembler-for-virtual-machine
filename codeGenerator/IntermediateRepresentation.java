1316ca17d2f452f306adb645de80c95b2854bdc3
Maryam Gilsenan <maryamgilsenan@gmail.com>
2021-01-10 20:47:04 -0500

    added IR

---
+++ codeGenerator/IntermediateRepresentation.java
@@ -0,0 +1,30 @@
+package codeGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntermediateRepresentation {
+    private ArrayList<String> machineCode;
+    private ArrayList<String> assemblyCode;
+
+    public IntermediateRepresentation() {
+        machineCode = new ArrayList<String>();
+        assemblyCode = new ArrayList<String>();
+    }
+
+    public List<String> getmachineCode(){
+        return machineCode;
+    }
+
+    public List<String> assemblyCode(){
+        return assemblyCode;
+    }
+
+    public void addMachineCode(String hex){
+        machineCode.add(hex);
+    }
+
+    public void addAssemblyCode(String instruction){
+        assemblyCode.add(instruction);
+    }
+}
